d31858fd167ad0f445e9f52a2cd46fedde89f224
qiangzisujun <3436833@qq.com>
2019-01-24 18:08:24 +0800

    添加服务类

--- consumer-demo/src/main/java/com/he/model/User.java
+++ consumer-demo/src/main/java/com/he/model/User.java
@@ -1,16 +1,11 @@
-package com.liu.model;
+package com.he.model;
+
 import lombok.Data;
-import tk.mybatis.mapper.annotation.KeySql;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
-@Table(name="tb_user")
 @Data
 public class User implements Serializable {
-    @Id
-    @KeySql(useGeneratedKeys = true)
     private Long id;
     private String userName;
     private String password;
--- user-service/src/main/java/com/liu/UserApplication.java
+++ user-service/src/main/java/com/liu/UserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.liu.mapper")
 public class UserApplication {
